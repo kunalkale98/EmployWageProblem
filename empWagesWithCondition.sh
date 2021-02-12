0de7df58bc303fae4788e0b2aebec3146348a768
kunalkale98 <kunal,k2898@gmail.com>
2021-02-12 14:13:12 +0530

    Added: Emp Wages till a condition of total working hrs or days is reached

---
+++ empWagesWithCondition.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+emp_rate_per_hr=20
+is_full_time=1
+is_part_time=2
+num_of_working_days=20
+max_hrs_in_month=100
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $max_hrs_in_month && $totalWorkingDays -lt $num_of_working_days ]]
+do
+   ((totalWorkingDays++))
+   empCheck=$(( RANDOM%3 ))
+   case $empCheck in
+      $is_full_time)
+         empHrs=8
+         ;;
+      $is_part_time)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$emp_rate_per_hr))
+echo "Total Salary: "$totalSalary
